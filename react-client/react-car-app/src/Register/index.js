4289c41ae14b54bb3d8c37596ef046c6d0b0e3cb
Home <home@Homes-MacBook.local>
2019-11-20 09:51:05 -0700

    back to working project with one model. Needs user auth and connection to an API

--- react-client/react-car-app/src/Register/index.js
+++ react-client/react-car-app/src/Register/index.js
@@ -1,54 +1,54 @@
-import React, {Component} from 'react'
-import { Form, Label, Button } from 'semantic-ui-react'
+// import React, {Component} from 'react'
+// import { Form, Label, Button } from 'semantic-ui-react'
 
-class Register extends Component {
-    constructor() {
-        super();
+// class Register extends Component {
+//     constructor() {
+//         super();
 
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
-
-
-  handleChange = (e) => {
-      e.preventDefault();
-      this.setState ({
-          [e.currentTarget.name]: e.currentTarget.value
-      })
-  }
+//         this.state = {
+//             email: '',
+//             password: ''
+//         }
+//     }
 
 
-  handleSubmit = async (e) => {
-    e.preventDefault()
-    console.log('Email & Password:', this.state)
-    const registrationUrl = 'process.env.REACT_APP_API_URL + /api/v1/dogs/';
-    const registerResponse = await fetch(registrationUrl, {
-        method: 'POST',
-        body: JSON.stringify(this.state),
-        credentials: 'include', // Send a session cookie along wit our request
-        headers: {
-            'Content-Type': 'application/json'
-        }
+//   handleChange = (e) => {
+//       e.preventDefault();
+//       this.setState ({
+//           [e.currentTarget.name]: e.currentTarget.value
+//       })
+//   }
+
+
+//   handleSubmit = async (e) => {
+//     e.preventDefault()
+//     console.log('Email & Password:', this.state)
+//     const registrationUrl = 'process.env.REACT_APP_API_URL + /api/v1/dogs/';
+//     const registerResponse = await fetch(registrationUrl, {
+//         method: 'POST',
+//         body: JSON.stringify(this.state),
+//         credentials: 'include', // Send a session cookie along wit our request
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
         
-    })
+//     })
 
-    const parsedResponse = await registerResponseResponse.json();
+//     const parsedResponse = await registerResponseResponse.json();
 
-    if (parsedResponse.status.code == 201) {
-        console.log('Sign up successful');
-        this.props.history.push('/cars')
-    }
-}
+//     if (parsedResponse.status.code == 201) {
+//         console.log('Sign up successful');
+//         this.props.history.push('/cars')
+//     }
+// }
 
-    render() {
-        return (
-            <div>
+//     render() {
+//         return (
+//             <div>
 
-            </div>
-        )
-    }
-}
+//             </div>
+//         )
+//     }
+// }
 
-export default Register;
+// export default Register;
